37c129da0e69aff978f3f81df2a9efd5ad8e15c0
Markus Hauge <markus.hauge@hotmail.com>
2019-10-22 15:41:14 +0200

    Subtract two hours from calendar time (temporary fix)

--- api/calendar.js
+++ api/calendar.js
@@ -1,11 +1,12 @@
-const ical = require('ical-generator');
-const moment = require('moment');
+const ical = require("ical-generator");
+const moment = require("moment");
 
 const createEvent = event => {
   const { id, title, teaser, companyName, date } = event;
-  const description = (teaser && teaser + '\n\n') + `https://ifinavet.no/event/${id}`;
-  const start = moment(date, 'DD.MM.YYYY HH:mm');
-  const end = start.clone().add(1, 'h');
+  const description =
+    (teaser && teaser + "\n\n") + `https://ifinavet.no/event/${id}`;
+  const start = moment(date, "DD.MM.YYYY HH:mm").subtract(2, "h");
+  const end = start.clone().add(1, "h");
 
   return {
     uid: id,
